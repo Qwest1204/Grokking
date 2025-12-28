7ed134c8f9669afc3a0ebe12b8315c182b58b87e
Qwest1204 <myPCwithUbuntu@gmail.com>
2025-12-29 00:17:33 +0300

    add PE (default sin/cos)

--- model/model.py
+++ model/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 from model.attention import MHA
-from model.modules import MLP, ProjectionLayer, ResidualConnection, InputEmbedding
+from model.modules import MLP, ProjectionLayer, ResidualConnection, InputEmbedding, PositionalEncoding
 
 class GrokkingTransformer(nn.Module):
     """
@@ -13,16 +13,17 @@ class GrokkingTransformer(nn.Module):
         self.embedding = InputEmbedding(d_model, vocab_size=vocab_size)
         self.mha = MHA(d_model, num_heads, dropout)
         self.mlp = MLP(d_model, d_ff, dropout)
+        self.pe = PositionalEncoding(d_model, max_len=4)
         self.residual_connection = ResidualConnection(dropout)
         self.projection = ProjectionLayer(d_model, vocab_size)
 
     def forward(self, x):
         x = self.embedding(x)
+        x = self.pe(x)
         x, att = self.mha(x, x, x)
         x = self.residual_connection(x)
         x = self.mlp(x)
         x = self.residual_connection(x)
         x_proj = self.projection(x)
         logits = x_proj[:, -1, :]
-        x = torch.log_softmax(logits, dim=-1)
-        return x, att
+        return logits, att
