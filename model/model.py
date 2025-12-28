9b512e55aaeb276644503c1de6f82fec153edc25
Qwest1204 <myPCwithUbuntu@gmail.com>
2025-12-29 01:44:39 +0300

    fix resirual connection

--- model/model.py
+++ model/model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 from model.attention import MHA
-from model.modules import MLP, ProjectionLayer, ResidualConnection, InputEmbedding, PositionalEncoding
+from model.modules import MLP, ProjectionLayer, LayerNormalization, InputEmbedding, PositionalEncoding
 
 class GrokkingTransformer(nn.Module):
     """
@@ -14,16 +14,19 @@ class GrokkingTransformer(nn.Module):
         self.mha = MHA(d_model, num_heads, dropout)
         self.mlp = MLP(d_model, d_ff, dropout)
         self.pe = PositionalEncoding(d_model, max_len=4)
-        self.residual_connection = ResidualConnection(dropout)
+        self.norm1 = LayerNormalization()
+        self.norm2 = LayerNormalization()
         self.projection = ProjectionLayer(d_model, vocab_size)
 
     def forward(self, x):
         x = self.embedding(x)
         x = self.pe(x)
         x, att = self.mha(x, x, x)
-        x = self.residual_connection(x)
+        x = x + self.dropout(x)
+        x = self.norm1(x)
         x = self.mlp(x)
-        x = self.residual_connection(x)
+        x = x + self.dropout(x)
+        x = self.norm2(x)
         x_proj = self.projection(x)
         logits = x_proj[:, -1, :]
         return logits, att
